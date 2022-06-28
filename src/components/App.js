5f4e247c9d530f8275842a6fbbce6ac70ac49b80
xnsei <banagetanmay@gmail.com>
2022-06-28 20:47:51 +0530

    adding basic blockchain structure

--- src/components/App.js
+++ src/components/App.js
@@ -27,6 +27,7 @@ class App extends Component {
           // Code...
           />
         )}
+        }
       </div>
     );
   }
---
+++ src/contracts/decentralizedSocialMedia.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0;
+
+contract decentralizedSocialMedia {
+  // Code goes here...
+}
